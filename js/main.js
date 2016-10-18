52cd34727544b844314c64c67ad13468f795a223
vassilis <vas.terzo@gmail.com>
2016-10-18 05:35:22 +0300

    update landing page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 
+
+$(document).ready(function(){
+  $('.slider').slick({
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    arrows: false,
+    fade: true,
+    cssEase: 'linear'
+  });
+});
